be8c7f13fca0b6fce4c1bf7651c08bd1cb85d914
Damian Koski <damian.koski97@gmail.com>
2023-07-15 09:55:02 +0200

    Get program version from csproj

--- src/Xo.Program/Program.cs
+++ src/Xo.Program/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using Xo.CodeGeneration;
 using Xo.Parsing;
 using Xo.Session;
 using Xo.SourceCode;
 
-const string version = "0.1.0-dev";
+var version = GetVersion();
 Console.WriteLine($"xo {version}");
 
 var stopwatch = new Stopwatch();
@@ -24,10 +25,39 @@ if (sourceFile is null)
 var session = new CompilationSession(sourceFile);
 var tokens = new TokenStream(session);
 var ast = Parser.Parse(tokens, session);
+if (session.Diagnostics.ErrorCount > 0)
+    TerminateCompilation(stopwatch, session.Diagnostics.ErrorCount);
+
+var parsingTime = stopwatch.Elapsed.TotalSeconds;
+
 var outputFileName = GetDefaultOutputFileName();
 CodeGenerator.GenerateExecutable(outputFileName, ast, session);
 
-Console.WriteLine($"Compilation finished in {stopwatch.Elapsed.TotalSeconds}s");
+var codeGenerationTime = stopwatch.Elapsed.TotalSeconds - parsingTime;
+
+Console.WriteLine($"Compilation finished in {stopwatch.Elapsed.TotalSeconds:0.0000000}s");
+Console.WriteLine($"  Parsing time: {parsingTime:0.0000000}s");
+Console.WriteLine($"  Code generation time: {codeGenerationTime:0.0000000}s");
+
+static string GetVersion()
+{
+    var assembly = Assembly.GetExecutingAssembly();
+    var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+    var version = versionInfo.ProductVersion!;
+    return version;
+}
+
+static void TerminateCompilation(Stopwatch compilationStopwatch, int errorCount)
+{
+    Console.WriteLine($"Compilation finished in {compilationStopwatch.Elapsed.TotalSeconds}s");
+    if (errorCount > 0)
+    {
+        var errorsS = errorCount == 1 ? "" : "s";
+        Console.WriteLine($"  with {errorCount} error{errorsS}");
+    }
+
+    Environment.Exit(1);
+}
 
 static string GetDefaultOutputFileName() =>
     RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "program.exe" : "program";
